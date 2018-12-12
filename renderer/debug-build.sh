6064241e93b2c170ad825e529282cb13e703bb93
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-12-12 13:51:12 -0800

    fix opensource checkup

--- renderer/debug-build.sh
+++ renderer/debug-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 if [ `uname` = 'Darwin' ]; then
 	IS_MAC="1"
